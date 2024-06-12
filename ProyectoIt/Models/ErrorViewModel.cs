211c0fbe5aaa6aad2f60748ac913a56b007c0d0b
Marcio Abriola <marcioabriola@gmail.com>
2024-06-12 20:29:33 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoIt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
