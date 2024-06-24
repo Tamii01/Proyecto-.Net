87936766cf5735cb4923cfb3da016e4966a05056
Marcio Abriola <marcioabriola@gmail.com>
2024-06-24 20:21:58 -0300

    subo clase

--- ProyectoIt/Controllers/LoginController.cs
+++ ProyectoIt/Controllers/LoginController.cs
@@ -3,16 +3,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ProyectoIt.Controllers
 {
-    public class LoginController : Controller
-    {
-        public ActionResult Login()
-        {
-            return View();
-        }
+	public class LoginController : Controller
+	{
 
-        public ActionResult Logout() 
-        {
-            return RedirectToAction("Login", "Login");
-        }
-    }
+		public ActionResult CrearCuenta()
+		{
+			return View();
+		}
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		public ActionResult LoginGoogle()
+		{
+			return RedirectToAction("Index", "Home");
+		}
+
+		public ActionResult Logout()
+		{
+			return RedirectToAction("Login", "Login");
+		}
+
+		public ActionResult OlvidoClave()
+		{
+			return View();
+		}
+
+		public ActionResult RecuperarCuenta()
+		{
+			return View();
+		}
+
+		public ActionResult EnviarMail()
+		{
+			return RedirectToAction("RecuperarCuenta", "Login");
+		}
+	}
 }
